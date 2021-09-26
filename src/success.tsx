39401544fa4dc1d7200256d7c84e1ff4200f0f85
AmieLauren <64654752+AmieLauren@users.noreply.github.com>
2021-09-26 11:25:00 -0500

    success page push with getter so can show appData in response. Could use more styling, but does min required work.

--- src/success.tsx
+++ src/success.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import Header from "./success-header";
 
 export const SuccessPage = (props) => {
+
+  const borrower = props.getLoanAppData().firstName;
+  //to help see that the app data was retrieved for success page. 
+  console.log(props.getLoanAppData());
+
   return (
-    <Header></Header>
+    <div>
+      <Header></Header>
+
+      <div>Congrats {borrower}, you submitted the form successfully! </div>
+
+    </div>
+
   );
 
 }
